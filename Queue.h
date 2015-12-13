d5be8e29000c9c67463757406bfa813aaba7e8eb
Andrey Morenkov <jedi-andr@mail.ru>
2015-12-14 00:23:29 +0300

    Code is compiling

--- Queue.h
+++ Queue.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <iostream>
+using namespace std;
 // Структуры данных - очередь
 
 template <class T>
@@ -12,52 +13,40 @@ private:
 	T* mas;       // массив очереди (сама очередь)
 	int maxsize;  // макс размер кольца очереди
 public:
-	TQueue(int _maxsize = 10)
-	{
-		maxsize = _maxsize;
-		mas = new T[maxsize];
-		begin = 0;
-		end = -1;
-		len = 0;
-	}
-	TQueue(const TQueue& Q)
-	{
-		maxsize = Q.maxsize;
-		mas = new T[maxsize];
-		for (int i = 0; i < maxsize; i++)
-			mas[i] = Q.mas[i];
-		begin = Q.begin;
-		end = Q.end;
-		len = Q.len;
-	}
-	~TQueue<T>()
-	{
-		delete[] mas;
-	}
+	TQueue(int _maxsize = 10);
+	TQueue(const TQueue& Q);
+	~TQueue<T>(){delete[] mas;}
 
 	// Операции
 	bool IsEmpty();       // Пусто?
 	bool IsFull();        // Полная?
 	void Push(const T n); // Добавить элемент в очередь
 	T Pop();              // Извлечь из очереди
-
-	// доступ к полям
-	int GetBegin() { return begin; }
-	int GetEnd(){ return end; }
-	int GetLen() { return len; }
-	int GetMaxsize() { return maxsize; }
-
-	// вывод
-	friend ostream& operator<<(ostream &out, const TQueue& Q)
-	{
-		for (int i = 0; i < Q.GetMaxsize; i++)
-			out << Q.mas[i];
-		return out;
-	}
 };
+template <class T>
+TQueue<T>::TQueue(int _maxsize)
+{
+	maxsize = _maxsize;
+	mas = new T[maxsize];
+	begin = 0;
+	end = -1;
+	len = 0;
+}
 
 template <class T>
-bool TQueue::IsEmpty()
+TQueue<T>::TQueue(const TQueue& Q)
+{
+	maxsize = Q.maxsize;
+	mas = new T[maxsize];
+	for (int i = 0; i < maxsize; i++)
+		mas[i] = Q.mas[i];
+	begin = Q.begin;
+	end = Q.end;
+	len = Q.len;
+}
+
+template <class T>
+bool TQueue<T>::IsEmpty()
 {
 	if (len == 0)
 		return true;
@@ -65,7 +54,7 @@ bool TQueue::IsEmpty()
 }
 
 template <class T>
-bool TQueue::IsFull()
+bool TQueue<T>::IsFull()
 {
 	if (len == maxsize)
 		return true;
@@ -73,7 +62,7 @@ bool TQueue::IsFull()
 }
 
 template <class T>
-void Push(const T n)
+void TQueue<T>::Push(const T n)
 {
 	if (end < (maxsize - 1))
 		end++;
@@ -84,7 +73,7 @@ void Push(const T n)
 }
 
 template <class T>
-T Pop()
+T TQueue<T>::Pop()
 {
 	if (IsEmpty())
 		throw - 1;
